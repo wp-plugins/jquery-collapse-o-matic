e1851352993b01728dcc4d9de192c2bca6832608
baden03 <baden03@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-04-09 09:35:49 +0000

    tagging version 1.3.8
    git-svn-id: https://plugins.svn.wordpress.org/jquery-collapse-o-matic/tags/1.3.8@370888 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- collapse.js
+++ collapse.js
@@ -1,8 +1,8 @@
 /*!
- * jQuery Collapse-O-Matic v1.1
+ * jQuery Collapse-O-Matic v1.2.3
  * http://www.twinpictures.de/collapse-o-matic/
  *
- * Copyright 2010, Twinpictures
+ * Copyright 2011, Twinpictures
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -25,6 +25,13 @@
  */
 
 jQuery(document).ready(function() {
+    
+    //inital collapse
+    jQuery('.collapseomatic:not(.close)').each(function(index) {
+	    var thisid = jQuery(this).attr('id');
+	    jQuery('#target-'+thisid).css('display', 'none');
+    });
+    
     jQuery('.collapseomatic').hover(function () {
             jQuery(this).addClass("hover");
         },
@@ -34,22 +41,33 @@ jQuery(document).ready(function() {
     );
     
     jQuery('.collapseomatic').click(function() {
-		//alert('phones ringin dude');
-		jQuery(this).toggleClass('close');
-		var id = jQuery(this).attr('id');
-		jQuery('#target-'+id).slideToggle('fast', function() {
-		    // Animation complete.
-		});
+	//alert('phones ringin dude');
+	jQuery(this).toggleClass('close');
+	//check if the title needs to be swapped out
+	var title = jQuery(this).attr('title');
+	var htmlstr = jQuery(this).html();
+	if(title != htmlstr){
+	    jQuery(this).attr('title', htmlstr);
+	    jQuery(this).html(title);
+	}
+	var id = jQuery(this).attr('id');
+	jQuery('#target-'+id).slideToggle('fast', function() {
+	    // Animation complete.
+	});
         
         //deal with grouped items if needed
         if(jQuery(this).attr('rel') !== undefined){
             var rel = jQuery(this).attr('rel');
-            closeOthers(rel);
+	    if(rel.indexOf('-highlander') != '-1'){
+		closeOtherMembers(rel, id);
+	    }
+            else{
+		closeOtherGroups(rel);
+	    }   
         }
-        
-	});
+    });
     
-    function closeOthers(rel){
+    function closeOtherGroups(rel){
         jQuery('.collapseomatic[rel!="' + rel +'"]').each(function(index) {
             //add close class if open
             if(jQuery(this).hasClass('close') && jQuery(this).attr('rel') !== undefined){                
@@ -62,10 +80,43 @@ jQuery(document).ready(function() {
         });
     }
     
+    function closeOtherMembers(rel, id){
+        jQuery('.collapseomatic[rel="' + rel +'"]').each(function(index) {
+            //add close class if open
+            if(jQuery(this).attr('id') != id && jQuery(this).hasClass('close') && jQuery(this).attr('rel') !== undefined){                
+                jQuery(this).removeClass('close');
+                var thisid = jQuery(this).attr('id');
+                jQuery('#target-'+thisid).slideToggle('fast', function() {
+                    // Animation complete.
+                });
+            }
+        });
+    }
+    
     var myFile = document.location.toString();
     if (myFile.match('#')) { // the URL contains an anchor
         // click the navigation item corresponding to the anchor
         var myAnchor = '#' + myFile.split('#')[1];
         jQuery(myAnchor).click();
     }
+    
+    jQuery('.expandall').click(function() {
+	jQuery('.collapseomatic:not(.close)').each(function(index) {	              
+	    jQuery(this).addClass('close');
+	    var thisid = jQuery(this).attr('id');
+	    jQuery('#target-'+thisid).slideToggle('fast', function() {
+		// Animation complete.
+	    });
+	});
+    });
+    
+    jQuery('.collapseall').click(function() {
+	jQuery('.collapseomatic.close').each(function(index) {	                 
+	    jQuery(this).removeClass('close');
+	    var thisid = jQuery(this).attr('id');
+	    jQuery('#target-'+thisid).slideToggle('fast', function() {
+		// Animation complete.
+	    });
+	});
+    });
 });
