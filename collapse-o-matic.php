063b43501f360cdc8e86596fa9305a2b7bcd191c
baden03 <baden03@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-11-15 03:49:06 +0000

    tagging version 1.3.11
    git-svn-id: https://plugins.svn.wordpress.org/jquery-collapse-o-matic/tags/1.3.11@462933 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- collapse-o-matic.php
+++ collapse-o-matic.php
@@ -3,7 +3,7 @@
 Plugin Name: jQuery Collapse-O-Matic
 Plugin URI: http://www.twinpictures.de/jquery-collapse-o-matic-1-3/
 Description: Collapse-O-Matic adds an `[expand]` shortcode that wraps content into a lovely, jQuery collapsible div.
-Version: 1.3.8
+Version: 1.3.11
 Author: Twinpictures
 Author URI: http://www.twinpictures.de
 License: GPL2
@@ -30,11 +30,11 @@ wp_enqueue_script('jquery');
 $plugin_url = trailingslashit( get_bloginfo('wpurl') ).PLUGINDIR.'/'. dirname( plugin_basename(__FILE__) );
 if (!is_admin()){
                 //collapse script
-                wp_register_script('collapseomatic-js', $plugin_url.'/collapse.js', array ('jquery'), '1.2.3' );
+                wp_register_script('collapseomatic-js', $plugin_url.'/collapse.js', array ('jquery'), '1.2.5' );
                 wp_enqueue_script('collapseomatic-js');
 	
 	//css
-	wp_register_style( 'collapseomatic-css', $plugin_url.'/style.css', array (), '1.3' );    
+	wp_register_style( 'collapseomatic-css', $plugin_url.'/style.css', array (), '1.4' );    
                 wp_enqueue_style( 'collapseomatic-css' );
 }
         
@@ -52,32 +52,57 @@ function collapsTronic($atts, $content = null){
 		'targclass' => '',
 		'rel' => '',
 		'expanded' => '',
+		'excerpt' => '',
+		'excerptpos' => 'below-trigger',
+		'excerpttag' => 'div',
+		'excerptclass' => '',
 	), $atts));
 	
+	if($excerpt){
+		if($excerptpos == 'above-trigger'){
+			$nibble = '<'.$excerpttag.' class="'.$excerptclass.'">'.$excerpt.'</'.$excerpttag.'>';
+		}
+		else{
+			$nibble = '<'.$excerpttag.' class="collapseomatic_excerpt '.$excerptclass.'">'.$excerpt.'</'.$excerpttag.'>';
+		}
+		
+	}
 	$altatt = '';
 	if($alt){
-		$altatt = 'alt="'.$alt.'"';
+		$altatt = 'alt="'.$alt.'" title="'.$alt.'"';
+	}
+	else{
+		$altatt = 'title="'.$title.'"';
 	}
 	$relatt = '';
 	if($rel){
 		$relatt = 'rel="'.$rel.'"';
 	}
 	if($expanded){
-		$trigclass .= ' close';
+		$trigclass .= ' colomat-close';
 	}
-	$link = '<'.$tag.' class="collapseomatic '.$trigclass.'" title="';
+	$link = '<'.$tag.' class="collapseomatic '.$trigclass.'" id="'.$id.'" '.$relatt.' '.$altatt.'>'.$title.'</'.$tag.'>';
 	if($swaptitle){
-		$link .= $swaptitle;
+		$link .= '<'.$tag.' id="swap-'.$id.'" style="display:none;">'.$swaptitle.'</'.$tag.'>';
 	}
-	else{
-		$link .= $title;
-	}
-	$link .= '" id="'.$id.'" '.$relatt.' '.$altatt.'>'.$title.'</'.$tag.'>';
 	$eDiv = '';
 	if($content != ' '){
 		$eDiv = '<div id="target-'.$id.'" class="collapseomatic_content '.$targclass.'">'.do_shortcode($content).'</div>';
 	}
-	return $link . $eDiv;
+	if($excerpt){
+		if($excerptpos == 'above-trigger'){
+			return $nibble . $link . $eDiv;
+		}
+		else if($excerptpos == 'below-trigger'){
+			return $link . $nibble . $eDiv;
+		}
+		else{
+			return $link . $eDiv . $nibble;
+		}
+	}
+	else{
+		return $link . $eDiv;
+	}
 }
 
 add_shortcode('expand', 'collapsTronic');
