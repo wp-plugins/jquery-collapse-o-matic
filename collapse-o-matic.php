ed46bf504050df5b1ccbad03c9d6b62f368431ef
baden03 <baden03@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-02-21 11:24:35 +0000

    tagging version 1.3.17
    git-svn-id: https://plugins.svn.wordpress.org/jquery-collapse-o-matic/tags/1.3.17@508177 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- collapse-o-matic.php
+++ collapse-o-matic.php
@@ -3,13 +3,14 @@
 Plugin Name: jQuery Collapse-O-Matic
 Plugin URI: http://www.twinpictures.de/jquery-collapse-o-matic-1-3/
 Description: Collapse-O-Matic adds an `[expand]` shortcode that wraps content into a lovely, jQuery collapsible div.
-Version: 1.3.8
+Version: 1.3.17
 Author: Twinpictures
-Author URI: http://www.twinpictures.de
+Author URI: http://twinpictures.de/
 License: GPL2
+
 */
 
-/*  Copyright 2011 Twinpictures (www.twinpictures.de)
+/*  Copyright 2012 Twinpictures (www.twinpictures.de)
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License, version 2, as 
@@ -25,18 +26,30 @@ License: GPL2
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-wp_enqueue_script('jquery');
+function collapsTronicInit() {
+	wp_enqueue_script('jquery');
 
-$plugin_url = trailingslashit( get_bloginfo('wpurl') ).PLUGINDIR.'/'. dirname( plugin_basename(__FILE__) );
-if (!is_admin()){
-                //collapse script
-                wp_register_script('collapseomatic-js', $plugin_url.'/collapse.js', array ('jquery'), '1.2.3' );
-                wp_enqueue_script('collapseomatic-js');
+	$plugin_url = trailingslashit( get_bloginfo('wpurl') ).PLUGINDIR.'/'. dirname( plugin_basename(__FILE__) );
+	if (!is_admin()){
+		//collapse script
+		wp_register_script('collapseomatic-js', $plugin_url.'/collapse.js', array ('jquery'), '1.3.1' );
+		wp_enqueue_script('collapseomatic-js');
+
+			//css
+		wp_register_style( 'collapseomatic-css', $plugin_url.'/style.css', array (), '1.5.1' );
+		wp_enqueue_style( 'collapseomatic-css' );
+	}
+
+	add_shortcode('expand', 'collapsTronic');
 	
-	//css
-	wp_register_style( 'collapseomatic-css', $plugin_url.'/style.css', array (), '1.3' );    
-                wp_enqueue_style( 'collapseomatic-css' );
+	for ($i=1; $i<30; $i++) {
+		add_shortcode('expandsub'.$i, 'collapsTronic');
+	}
+
+	//add the filter to the sidebar widgets
+	add_filter('widget_text', 'do_shortcode');
 }
+add_action('init', 'collapsTronicInit');
         
 
 function collapsTronic($atts, $content = null){
@@ -50,40 +63,89 @@ function collapsTronic($atts, $content = null){
 		'tag' => 'span',
 		'trigclass' => '',
 		'targclass' => '',
+		'trigpos' => 'above',
 		'rel' => '',
 		'expanded' => '',
+		'excerpt' => '',
+		'excerptpos' => 'below-trigger',
+		'excerpttag' => 'div',
+		'excerptclass' => '',
 	), $atts));
 	
+	if($excerpt){
+		if($excerptpos == 'above-trigger'){
+			$nibble = '<'.$excerpttag.' class="'.$excerptclass.'">'.$excerpt.'</'.$excerpttag.'>';
+		}
+		else{
+			$nibble = '<'.$excerpttag.' class="collapseomatic_excerpt '.$excerptclass.'">'.$excerpt.'</'.$excerpttag.'>';
+		}
+		
+	}
 	$altatt = '';
 	if($alt){
-		$altatt = 'alt="'.$alt.'"';
+		$altatt = 'alt="'.$alt.'" title="'.$alt.'"';
+	}
+	else{
+		$altatt = 'title="'.$title.'"';
 	}
 	$relatt = '';
 	if($rel){
 		$relatt = 'rel="'.$rel.'"';
 	}
 	if($expanded){
-		$trigclass .= ' close';
+		$trigclass .= ' colomat-close';
 	}
-	$link = '<'.$tag.' class="collapseomatic '.$trigclass.'" title="';
+	$link = "<".$tag." class='collapseomatic ".$trigclass."' id='".$id."' ".$relatt." ".$altatt.">".$title."</".$tag.">\n";
 	if($swaptitle){
-		$link .= $swaptitle;
+		$link .= "<".$tag." id='swap-".$id."' style='display:none;'>".$swaptitle."</".$tag.">\n";
+	}
+	$eDiv = '';
+	if($content){
+		$eDiv = "<div id='target-".$id."' class='collapseomatic_content ".$targclass."'>".do_shortcode($content)."</div>\n";
+	}
+	if($excerpt){
+		if($excerptpos == 'above-trigger'){
+			if($trigpos == 'below'){
+				$retStr = $eDiv.$nibble.$link;
+			}
+			else{
+				$retStr = $nibble.$link.$eDiv;
+			}
+		}
+		else if($excerptpos == 'below-trigger'){
+			if($trigpos == 'below'){
+				$retStr =  $eDiv.$link.$nibble;
+			}
+			else{
+				$retStr = $link.$nibble.$eDiv;
+			}
+		}
+		else{
+			if($trigpos == 'below'){
+				$retStr = $eDiv.$link.$nibble;
+			}
+			else{
+				$retStr = $link.$eDiv.$nibble;
+			}
+		}
 	}
 	else{
-		$link .= $title;
+		if($trigpos == 'below'){
+			$retStr = $eDiv.$link;
+		}
+		else{
+			$retStr = $link.$eDiv;
+		}
 	}
-	$link .= '" id="'.$id.'" '.$relatt.' '.$altatt.'>'.$title.'</'.$tag.'>';
-	$eDiv = '';
-	if($content != ' '){
-		$eDiv = '<div id="target-'.$id.'" class="collapseomatic_content '.$targclass.'">'.do_shortcode($content).'</div>';
-	}
-	return $link . $eDiv;
+	return $retStr;
 }
 
+/*
 add_shortcode('expand', 'collapsTronic');
-add_shortcode('expandsub1', 'collapsTronic');
-add_shortcode('expandsub2', 'collapsTronic');
-add_shortcode('expandsub3', 'collapsTronic');
+for ($i=1; $i<30; $i++) {
+	add_shortcode('expandsub'.$i, 'collapsTronic');
+}
+*/
 
 //add the filter to the sidebar widgets
 add_filter('widget_text', 'do_shortcode');
